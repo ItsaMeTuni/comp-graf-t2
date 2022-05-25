a6accc6713a96bd3a4a9217dca5931b1945c1a05
Lucas Antunes <lucas.antunes@poatek.com>
2022-05-25 20:02:24 -0300

    implement global scaling of models

--- model.py
+++ model.py
@@ -3,7 +3,7 @@ from OpenGL.GLU import *
 from OpenGL.GL import *
 from random import uniform
 
-pixel_size = 1
+pixel_size = 0.5
 
 class Model:
 
@@ -20,14 +20,14 @@ class Model:
 
     def render(self):
 
+        glPushMatrix()
+        glScalef(pixel_size, pixel_size, pixel_size)
+
         glBegin(GL_QUADS)
         for x in range(self.width):
             for y in range(self.height):
                 pixel_color = self.pixels[y * self.width + x]
                 glColor3f(*self.colors[pixel_color])
-
-                local_x = (x - self.width / 2) * pixel_size
-                local_y = (y - self.height / 2) * pixel_size * -1
 
                 glVertex3f(-self.width/2 + x, -self.height/2 + (self.height - y), 0)
                 glVertex3f(-self.width/2 + x + 1, -self.height/2 + (self.height - y), 0)
@@ -35,5 +35,6 @@ class Model:
                 glVertex3f(-self.width/2 + x, -self.height/2 + (self.height - y - 1), 0)
         glEnd()
 
+        glPopMatrix()
 
 player_model = Model(3, 3, [(0, 0, 0), (1, 0, 0)], [0, 1, 0, 1, 1, 1, 1, 1, 1])
